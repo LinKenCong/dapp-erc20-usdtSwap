3d8ce140d2e3c8b08299e206c83bd4beb04fa40d
kyrie <lamkencong@gmail.com>
2023-03-13 22:48:31 +0800

    update

--- src/Token.sol
+++ src/Token.sol
@@ -5,6 +5,6 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract Token is ERC20("ZROs", "ZROs") {
     constructor() {
-        _mint(0xbB625390769470B667A91646b96780854b29a54e, 1000000000 * 10 ** decimals());
+        _mint(msg.sender, 1000000000 * 10 ** decimals());
     }
 }
