318dbf8d009a9a5e2126ac4dd4c98cb3b12618c7
kyrie <lamkencong@gmail.com>
2023-03-17 02:16:08 +0800

    add ProgressBar component

--- frontend/components/ProgressBar/index.tsx
+++ frontend/components/ProgressBar/index.tsx
@@ -1,11 +1,45 @@
+import type { NextComponentType } from "next";
+import { Progress } from "antd";
 import style from "./style.module.scss";
+import { useState } from "react";
+import { TOKEN_INFO_MAP } from "../../constants";
 
-const ProgressBar = () => {
+const ProgressBar: NextComponentType = () => {
+  const TokenSymbol = TOKEN_INFO_MAP.swapToken.symbol;
+  const [maxToken, setMaxToken] = useState("1000");
+  const [sellToken, setSellToken] = useState("22.2");
+  const [buyToken, setBuyToken] = useState("250.5");
+
+  const countPercent = (buy: number | string, max: number | string): number => {
+    buy = Number(buy);
+    max = Number(max);
+    const sell = max - buy;
+    const str = (Math.round((sell / max) * 10000) / 100).toString();
+    const index = str.indexOf(".");
+    const res = Number(str.substring(0, index == -1 ? str.length : index));
+    return res;
+  };
+
   return (
     <>
-      <div className="ProgressBar">
-        
-      </div>
+      <aside className={style.ProgressBar}>
+        <Progress
+          percent={countPercent(buyToken, maxToken)}
+          showInfo={false}
+          strokeColor={"#FE9A2E"}
+          trailColor={"#000"}
+        />
+        <div className={style.sellInfo}>
+          <span>
+            {maxToken}
+            <span className={style.symbol}>{TokenSymbol}</span>
+          </span>
+          <span>
+            {buyToken}
+            <span className={style.symbol}>{TokenSymbol}</span>
+          </span>
+        </div>
+      </aside>
     </>
   );
 };
