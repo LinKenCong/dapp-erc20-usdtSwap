5e893ce35e19c5e88995d4b3d799945cf40a8cd6
kyrie <lamkencong@gmail.com>
2023-03-17 01:19:22 +0800

    add social icon link

--- frontend/components/SocialUrlList/index.tsx
+++ frontend/components/SocialUrlList/index.tsx
@@ -1,30 +1,37 @@
+import { NextComponentType } from "next";
 import Image from "next/image";
 import icon_discord from "../../assets/icon/discord.svg";
 import icon_twitter from "../../assets/icon/twitter.svg";
 import icon_youtube from "../../assets/icon/youtube.svg";
 import icon_telegram from "../../assets/icon/telegram.svg";
 import style from "./style.module.scss";
+import { SOCIAL_URL_LIST } from "../../constants";
+import Link from "next/link";
 
 const IconList = [
   {
     img: icon_twitter,
     bgColor: "#E0F2F7",
+    url: SOCIAL_URL_LIST.twitter,
   },
   {
     img: icon_discord,
     bgColor: "#F2EFFB",
+    url: SOCIAL_URL_LIST.discord,
   },
   {
     img: icon_youtube,
     bgColor: "#F8E0E0",
+    url: SOCIAL_URL_LIST.youtube,
   },
   {
     img: icon_telegram,
     bgColor: "#E0F8F7",
+    url: SOCIAL_URL_LIST.telegram,
   },
 ];
 
-const SocialUrlList = () => {
+const SocialUrlList: NextComponentType = () => {
   const IconItem = (url: any) => <Image src={url} alt="icon" />;
 
   return (
@@ -32,8 +39,10 @@ const SocialUrlList = () => {
       <div className={style.SocialUrlList}>
         <ul>
           {IconList.map((item: any, index) => (
-            <li style={{ backgroundColor: item.bgColor }}>
-              <Image src={item.img} key={index} alt="icon" />
+            <li style={{ backgroundColor: item.bgColor }} key={index}>
+              <a href={item.url} target={"_blank"}>
+                <Image src={item.img} alt="icon" />
+              </a>
             </li>
           ))}
         </ul>
