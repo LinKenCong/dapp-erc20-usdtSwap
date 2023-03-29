b7db41dd6a21c583527cc63655f065426197fe12
kyrie <lamkencong@gmail.com>
2023-03-29 16:25:46 +0800

    add load component

---
+++ frontend/components/NetwrokLoading.tsx
@@ -0,0 +1,25 @@
+import { NextComponentType } from "next";
+import { LoadingOutlined } from "@ant-design/icons";
+const loadIcon = <LoadingOutlined style={{ fontSize: 24 }} spin />;
+
+const NetwrokLoading: NextComponentType = () => {
+  return (
+    <>
+      <div
+        style={{
+          padding: "2rem 0",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        {loadIcon}
+        <span
+          style={{ paddingLeft: "1rem", fontSize: "0.8rem" }}
+        >{`Netwrok Loading...`}</span>
+      </div>
+    </>
+  );
+};
+
+export default NetwrokLoading;
