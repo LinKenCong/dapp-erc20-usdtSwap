1d1f5de22b8b40a660a123c5965bef00ea88546d
kyrie <lamkencong@gmail.com>
2023-03-29 16:24:47 +0800

    update constant

--- frontend/constants/index.ts
+++ frontend/constants/index.ts
@@ -1,4 +1,5 @@
 import ABI_ERC20 from "./abi/ERC20.json";
+import ABI_SwapToken from "./abi/SwapToken.json";
 
 export const COUNTDOWN = {
   deadline: 1680190038000, // UTC
@@ -6,6 +7,7 @@ export const COUNTDOWN = {
 
 export const ABI = {
   ERC20: ABI_ERC20,
+  SwapToken: ABI_SwapToken,
 };
 
 export enum SupportedChainId {
@@ -14,18 +16,18 @@ export enum SupportedChainId {
 }
 
 export const CONTRACT_SWAPTOKEN_MAP = {
-  [SupportedChainId.BSC]: "0x000000000000000000000000000000000000main",
-  [SupportedChainId.BSC_TESTNET]: "0x000000000000000000000000000000000000test",
+  [SupportedChainId.BSC]: "0x43b27367EB8c5A2c25134cB2E40Cee765C2Edb39",
+  [SupportedChainId.BSC_TESTNET]: "0xC818CE8580cC0fd0D4065dE6259BfFf1bFA7e071",
 };
 
 export const CONTRACT_USDT_MAP = {
   [SupportedChainId.BSC]: "0x55d398326f99059fF775485246999027B3197955",
-  [SupportedChainId.BSC_TESTNET]: "0x000000000000000000000000000000000000test",
+  [SupportedChainId.BSC_TESTNET]: "0x70AF9f834E448C1a115Ea98cc151A315e6538248",
 };
 
 export const TOKEN_INFO_MAP = {
   swapToken: {
-    name: "Token name",
+    name: "ZRO Token",
     symbol: "ZRO",
     decimals: "18",
   },
@@ -43,9 +45,12 @@ export const SOCIAL_URL_LIST = {
   telegram: "https://www.google.com/",
 };
 
+export const CURRENT_CHAINID = 97;
+
 export const CURRENT_CONTRACT = {
-  swaptoken:
-    process.env.NEXT_PUBLIC_ENABLE_TESTNETS === "true"
-      ? CONTRACT_SWAPTOKEN_MAP[97]
-      : CONTRACT_SWAPTOKEN_MAP[56],
+  swaptoken: CONTRACT_SWAPTOKEN_MAP[CURRENT_CHAINID],
+  usdt: CONTRACT_USDT_MAP[CURRENT_CHAINID],
 };
+
+export const CURRENT_CONTRACT_SWAPTOKEN = "0x8d2eA84191b5484fF4d550ECCF5a5E3E3351cc6C";
+export const CURRENT_CONTRACT_USDT = "0x70AF9f834E448C1a115Ea98cc151A315e6538248";
