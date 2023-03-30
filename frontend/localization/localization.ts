53b6ed2f705f8f07ba735c2dd075c7bd0b8275fa
kyrie <lamkencong@gmail.com>
2023-03-31 06:51:38 +0800

    del package

--- frontend/localization/localization.ts
+++ frontend/localization/localization.ts
@@ -1,35 +1,16 @@
 import i18next from "i18next";
 import { initReactI18next, useTranslation } from "react-i18next";
-import Backend from "i18next-chained-backend";
-import LocalStorageBackend from "i18next-localstorage-backend";
 
 const I18N_NAME_SPACE = "translation";
 
 i18next
   .use(initReactI18next)
-  .use(Backend)
   .init({
     resources: {},
-    lng: "en",
     fallbackLng: "en",
     ns: I18N_NAME_SPACE,
     interpolation: {
       escapeValue: false,
-    },
-    backend: {
-      backends: [
-        LocalStorageBackend, // primary backend
-      ],
-      backendOptions: [
-        {
-          // prefix for stored languages
-          prefix: "i18next_res_",
-          // expiration
-          expirationTime: 7 * 24 * 60 * 60 * 1000,
-          // can be either window.localStorage or window.sessionStorage. Default: window.localStorage
-          store: typeof window !== "undefined" ? window.localStorage : null,
-        },
-      ],
     },
   });
 
